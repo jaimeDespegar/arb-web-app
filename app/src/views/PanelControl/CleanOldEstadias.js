96bb9553154a09d541140a5f77ba6cee0633e0c3
jaimeDespegar <jaime.quispe.1994@outlook.com>
2020-11-30 06:52:40 -0300

    configuration

--- app/src/views/PanelControl/CleanOldEstadias.js
+++ app/src/views/PanelControl/CleanOldEstadias.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import GridItem from "components/Grid/GridItem";
 import GridContainer from "components/Grid/GridContainer";
 import Button from "components/CustomButtons/Button";
 import Card from "components/Card/Card";
+import CardBody from "components/Card/CardBody";
+import FormLabel from "@material-ui/core/FormLabel";
 import CardHeader from "components/Card/CardHeader";
 import CardFooter from "components/Card/CardFooter";
+import TextField from '@material-ui/core/TextField';
 import axios from "axios";
 import { headerAuthorization } from "./../../variables/token";
 
@@ -29,7 +32,8 @@ const useStyles = makeStyles(styles);
 export default function CleanOldStadias() {
   
   const classes = useStyles();
-  
+  const [seconds, setSeconds] = React.useState(60);
+
   const limpiarEstadias = () => {
     axios
       .get("http://127.0.0.1:8000/api/estadia/cleanOldStadias/", headerAuthorization())
@@ -41,6 +45,36 @@ export default function CleanOldStadias() {
       alert('Error al limpiar estadías anteriores')
     })
   }
+  
+  const loadConfiguration = () => {
+    axios
+      .get("http://127.0.0.1:8000/api/configuration/secondsPending/", headerAuthorization())
+      .then(response => {
+        if (response.data && response.data.configurationValue) {
+          setSeconds(response.data.configurationValue);
+        }
+      })
+    .catch((error) => { 
+      console.log('Error buscando la configuracion ', error);
+    })
+  }
+
+  const updateConfiguration = () => {
+    const data = {
+      value: seconds
+    }
+    axios
+      .put("http://127.0.0.1:8000/api/configuration/update/secondsPending/", data, headerAuthorization())
+      .then(response => {
+        alert('¡Valor actualizado!');
+      })
+    .catch((error) => { 
+      console.log('Error buscando la configuracion ', error);
+      alert('Error inesperado al actualizar la configuracion');
+    })
+  }
+
+  useEffect(() => { loadConfiguration() }, [])
 
   return (
     <div>
@@ -48,11 +82,29 @@ export default function CleanOldStadias() {
         <GridItem xs={12} sm={12} md={8}>
           <Card styles={{textAlign: 'center'}}>
             <CardHeader color="primary">
-              <h4 className={classes.cardTitleWhite}>Limpiar estadías</h4>
+              <h4 className={classes.cardTitleWhite}>Configuraciones</h4>
             </CardHeader>
-            
+            <CardBody>
+              <Button color="primary" onClick={() => limpiarEstadias()}>Limpiar Estadías previas</Button>
+            </CardBody>
             <CardFooter>
-              <Button color="primary" onClick={() => limpiarEstadias()}>limpiar</Button>
+              {/* <Button color="primary" onClick={() => limpiarEstadias()}>Limpiar estadías previas</Button> */}
+            </CardFooter>
+          </Card>
+          <Card styles={{textAlign: 'center'}}>
+            <CardHeader color="primary">
+              <h4 className={classes.cardTitleWhite}>Estadías</h4>
+            </CardHeader>
+            <CardBody>
+              <FormLabel style={{marginTop: 30, marginRight: 20}}>Tiempo para considerar pendientes </FormLabel>
+              <TextField
+                id="count-seconds" label="Segundos" type="number"
+                value={seconds} onChange={e=>setSeconds(e.target.value)}
+                InputLabelProps={{ shrink: true }}
+              />
+            </CardBody>
+            <CardFooter>
+              <Button color="primary" onClick={() => updateConfiguration()}>Guardar cambios</Button>
             </CardFooter>
           </Card>
         </GridItem>
